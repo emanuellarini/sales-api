0d9273c82850801fa4018d1c884926c86dd52e6a
emanuellarini <emanuellarini@hotmail.com>
2017-05-07 05:55:12 -0300

    [add] sql query event logging

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,7 +13,7 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
+        'Illuminate\Database\Events\QueryExecuted' => [
             'App\Listeners\EventListener',
         ],
     ];
