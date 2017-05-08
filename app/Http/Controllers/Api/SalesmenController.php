869b28013a4dd5c2e09489a6f7473ab7b156f54a
emanuellarini <emanuellarini@hotmail.com>
2017-05-08 08:00:21 -0300

    [remove] Unused dependency

--- app/Http/Controllers/Api/SalesmenController.php
+++ app/Http/Controllers/Api/SalesmenController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Salesman;
 use App\Repositories\Salesman\SalesmanRepository;
 use App\Transformers\SalesmanTransformer;
 use App\Transformers\UserTransformer;
